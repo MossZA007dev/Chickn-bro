cd4aac56fe18439eba4687c5a379aa7919e60f41
MossZA007dev <mossnulanoa@gmail.com>
2026-07-16 19:34:50 +0700

    Delete การเงิน chick bro directory

--- การเงิน chick bro/sw.js
+++
@@ -1,9 +0,0 @@
-const CACHE='chicknbro-v1';
-const ASSETS=[
-  '/index.html',
-  '/manifest.json',
-  '/icon.svg'
-];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))))});
